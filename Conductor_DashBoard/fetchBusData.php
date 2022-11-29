26d08b38bcec65ea764341514fbf46d830b6ec10
_27_abhi_ <abhinavkuppasad27@gmail.com>
2022-11-29 09:55:22 +0530

    Search bus from source and destination

--- Conductor_DashBoard/fetchBusData.php
+++ Conductor_DashBoard/fetchBusData.php
@@ -18,7 +18,7 @@ $fetchData = fetch_data($db, $tableName, $columns);
 function fetch_data($db, $tableName, $columns){
  if(empty($db)){
   $msg= "Database connection error";
- }elseif (empty($columns) || !is_array($columns)) {
+ }elseif (empty($columns) || !is_array($columns)) { 
   $msg="columns Name must be defined in an indexed array";
  }elseif(empty($tableName)){
    $msg= "Table Name is empty";
