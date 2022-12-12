f9cb686d7204832795df10d53331a44f52e70cfa
_27_abhi_ <abhinavkuppasad27@gmail.com>
2022-12-12 20:39:22 +0530

    milage check

--- Conductor_DashBoard/Milage.php
+++ Conductor_DashBoard/Milage.php
@@ -5,7 +5,7 @@ if ($_SESSION['status'] != "Active") {
   header("location:../Login/dist/login.php");
 }
 
-?>
+
 
 //database connection
 $hostName = "localhost";
@@ -15,41 +15,42 @@ $databaseName = "test4";
 $conn = new mysqli($hostName, $userName, $password, $databaseName);
 // Check connection
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
 
-$db= $conn;
-$tableName="trip_real_details";
-$columns= ['trip_no_real', 'fuel','arrival_time','departure_time','km_count'];
-//$busno=$_GET['BusNumber'];
+$db = $conn;
+$tableName = "trip_real_details";
+$columns = ['trip_no_real', 'fuel', 'arrival_time', 'departure_time', 'km_count'];
+$busno = $_GET['BusNumber'];
 $fetchData = fetch_data($db, $tableName, $columns);
-function fetch_data($db, $tableName, $columns){
-if(empty($db)){
-$msg= "Database connection error";
-}elseif (empty($columns) || !is_array($columns)) {
-$msg="columns Name must be defined in an indexed array";
-}elseif(empty($tableName)){
-$msg= "Table Name is empty";
-}else{
-//$columnName = implode(", ", $columns);
+function fetch_data($db, $tableName, $columns)
+{
+  if (empty($db)) {
+    $msg = "Database connection error";
+  } elseif (empty($columns) || !is_array($columns)) {
+    $msg = "columns Name must be defined in an indexed array";
+  } elseif (empty($tableName)) {
+    $msg = "Table Name is empty";
+  } else {
+    //$columnName = implode(", ", $columns);
 //joins
-$query1 = "SELECT * FROM `mileage`"; //mileage is a view
+    $query1 = "SELECT * FROM `mileage`"; //mileage is a view
 //$query="SELECT * FROM Milage ORDER BY DESC";
 //$db->query($query1);
-$result = $db->query($query1);
-if($result== true){
-if ($result->num_rows > 0) {
-$row= mysqli_fetch_all($result, MYSQLI_ASSOC);
-$msg= $row;
-} else {
-$msg= "No Data Found";
-}
-}else{
-$msg= mysqli_error($db);
-}
-}
-return $msg;
+    $result = $db->query($query1);
+    if ($result == true) {
+      if ($result->num_rows > 0) {
+        $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $msg = $row;
+      } else {
+        $msg = "No Data Found";
+      }
+    } else {
+      $msg = mysqli_error($db);
+    }
+  }
+  return $msg;
 }
 ?>
 
@@ -94,32 +95,58 @@ return $msg;
               </thead>
               <tbody>
                 <?php
-  if (is_array($fetchData)) {
-    $sn = 1;
-    foreach ($fetchData as $data) {
-  ?>
+                if (is_array($fetchData)) {
+                  $sn = 1;
+                  foreach ($fetchData as $data) {
+                ?>
                 <tr>
                   <td>
                     <?php echo $sn; ?>
                   </td>
                   <td>
-                    <?php echo $data['bus_no'] ?? ''; ?>
+                    <?php $busNum = $data['bus_no'] ?? '';
+                    if (!$busNum) {
+                      echo "bus number not found";
+                    } // if empty value fetched from database, echos bus no not found
+                    else {
+                      echo "$busNum";
+                    } ?>
+                  </td>
+                  <td>
+                    <?php if (!$busNum) {
+                      echo "bus number not found";
+                    } else {
+                      echo $data['MILAGE'] ?? '';
+                    } ?>
+                  </td>
+                </tr>
+                <!-- <tr>
+                  <td>
+                    <?php echo $sn; ?>
+                  </td>
+                  <td>
+
+                    <?php $busNum = $data['bus_no'] ?? '';
+                    if (!$busNum) {
+                      echo "<script>alert('incorrect bus number')</script>";
+                    }
+                    echo $data['bus_no'] ?? ''; ?>
                   </td>
                   <td>
                     <?php echo $data['MILAGE'] ?? ''; ?>
                   </td>
-                </tr>
+                </tr> -->
                 <?php
-      $sn++;
-    }
-  } else { ?>
+                    $sn++;
+                  }
+                } else { ?>
                 <tr>
                   <td colspan="8">
                     <?php echo $fetchData; ?>
                   </td>
                 <tr>
                   <?php
-  } ?>
+                } ?>
               </tbody>
             </table>
           </div>
