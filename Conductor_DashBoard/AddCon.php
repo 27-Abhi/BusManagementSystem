b8abcc8b693520ba42620266933cc01b00de416c
Navin-Morajkar <navinsika@gmail.com>
2022-12-14 00:52:00 +0530

    Minor

--- Conductor_DashBoard/AddCon.php
+++ Conductor_DashBoard/AddCon.php
@@ -15,6 +15,7 @@ if ($_SESSION['status'] != "Active") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" type="text/css" href="tripDetails.css">
 
     <title>Add Conductor </title>
